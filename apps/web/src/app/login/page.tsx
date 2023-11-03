9d8b5fd3491929a82182e228541f3a50dc6f4fdd
drmodun <jan.modun.st@gmail.com>
2023-11-03 16:44:15 +0100

    added first views and images for login form

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -1,15 +1,11 @@
 import { QueryClientWrapper } from "@/utility/wrappers/queryWrapper";
 import { SignInForm } from "components/SignInForm/SignInForm";
 import classes from "./page.module.scss";
+import GreenDrop from "@/views/GreenDrop/GreenDrop";
+import LoginView from "@/views/LoginView/LoginView";
 
 const LoginPage = () => {
-  return (
-    <QueryClientWrapper>
-      <div className={classes.container}>
-        <SignInForm />
-      </div>
-    </QueryClientWrapper>
-  );
+  return <LoginView />;
 };
 
 export default LoginPage;
