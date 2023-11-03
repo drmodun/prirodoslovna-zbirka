db397e4eaa7980a0e1b3f71bde081b52a7db39f9
drmodun <jan.modun.st@gmail.com>
2023-11-03 17:09:45 +0100

    base files

---
+++ apps/web/src/components/RegiterForm/RegisterForm.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useLogin } from "@/api/useLogin";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Input from "components/Input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import email from "assets/images/email.svg";
+import password from "assets/images/password.svg";
+import BaseButton from "components/BaseButton";
+import classes from "./SignInForm.module.scss";
+import Link from "next/link";
+import { ButtonColor } from "@/shared/enums";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+export const SignInForm = () => {
+  const schema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6).max(100),
+  });
+
+  const form = useForm({
+    resolver: zodResolver(schema),
+  });
+
+  const login = useLogin();
+
+  const onSubmit = async (data: any) => {
+    await login.mutateAsync(data);
+  };
+
+  return (
+    <form onSubmit={form.handleSubmit(onSubmit)} className={classes.form}>
+      <Input form={form} attribute="email" question="Email" image={email} />
+      <Input
+        form={form}
+        attribute="password"
+        question="Password"
+        isPassword
+        image={password}
+        isDisabled={login.isLoading}
+      />
+      <div className={classes.buttons}>
+        <BaseButton text="Register" />
+        <Link href="/regitration">
+          <BaseButton text="Sign up" initColor={ButtonColor.BLUE} />
+        </Link>
+      </div>
+    </form>
+  );
+};
