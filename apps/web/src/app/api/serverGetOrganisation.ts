108e23900e1f6153eb8fc169db6b14323d01fbe5
drmodun <jan.modun.st@gmail.com>
2024-01-10 17:53:47 +0100

    added server call for organisation

---
+++ apps/web/src/app/api/serverGetOrganisation.ts
@@ -0,0 +1,12 @@
+import { baseURL } from "./shared";
+
+export const serverGetOrganisation = async (id: string) => {
+  try {
+    const response = await fetch(`${baseURL}/organisations/${id}`, {
+      next: { revalidate: 3600 * 24, tags: ["organisations"] },
+    });
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
