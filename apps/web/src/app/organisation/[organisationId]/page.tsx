7bbb018d311e385e10fcce52f2c212dc9d9b3a9c
drmodun <jan.modun.st@gmail.com>
2024-01-10 18:03:34 +0100

    desktop version finished

--- apps/web/src/app/organisation/[organisationId]/page.tsx
+++ apps/web/src/app/organisation/[organisationId]/page.tsx
@@ -1,2 +1,18 @@
-const OrganisationPage = () => {
-    
+import { serverGetOrganisation } from "@/api/serverGetOrganisation";
+import { ExtendedOrganisationResponse } from "@biosfera/types";
+import classes from "./page.module.scss";
+import OrganisationHero from "components/OrganisationHero";
+
+const OrganisationPage = async ({ params }: { params: any }) => {
+  const organisationInfo: ExtendedOrganisationResponse =
+    await serverGetOrganisation(params.organisationId);
+
+  console.log(organisationInfo);
+  return (
+    <div>
+      <OrganisationHero organisation={organisationInfo} />
+    </div>
+  );
+};
+
+export default OrganisationPage;
