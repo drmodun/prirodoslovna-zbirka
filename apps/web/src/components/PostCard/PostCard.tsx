0fb415a955af671c91700271f4725979d9d5a4e7
drmodun <jan.modun.st@gmail.com>
2024-01-10 12:30:06 +0100

    completed styles and images for post card

--- apps/web/src/components/PostCard/PostCard.tsx
+++ apps/web/src/components/PostCard/PostCard.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { dateShortener } from "@/utility/static/dateShortener";
 import placeholder from "assets/images/placeholder.png";
 import defaultPic from "assets/images/lion.svg";
+import likeLeaf from "assets/images/like-leaf.svg";
 import Link from "next/link";
 
 export interface PostCardProps {
@@ -11,11 +12,19 @@ export interface PostCardProps {
 }
 
 //will a post have one or more pictures?
+//TODO: add functionality of like after backend implementation and user context
 
 export const PostCard = ({ post }: PostCardProps) => (
   <div className={classes.container}>
-    <div className={classes.image}>
-      <Image src={post.images[0]} alt={post.title} layout="fill" />
+    <div className={classes.upper}>
+      <div className={classes.leaf}>
+        <div className={classes.likeLeaf}>
+          <Image src={likeLeaf} alt="list za like" layout="fill" />
+        </div>
+      </div>
+      <div className={classes.image}>
+        <Image src={post.images[0]} alt={post.title} layout="fill" />
+      </div>
     </div>
     <div className={classes.content}>
       <span className={classes.date}>{dateShortener(post.updatedAt)}</span>
@@ -32,7 +41,7 @@ export const PostCard = ({ post }: PostCardProps) => (
           <Link href={`/user/${post.authorId}`} className={classes.authorName}>
             {post.authorName}
           </Link>
-          <Link href={`/exponat/${post.exponatId}`}>{post.exponatName}</Link>
+          <Link href={`/exponat/${post.exponatId}`}>vidi više {post.exponatName}</Link>
         </div>
       </div>
     </div>
