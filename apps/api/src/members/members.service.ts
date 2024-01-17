e95622b4a4b3f0524c7152738564b1ce1acb432e
drmodun <jan.modun.st@gmail.com>
2024-01-17 18:20:36 +0100

    added migration and added adming rights

--- apps/api/src/members/members.service.ts
+++ apps/api/src/members/members.service.ts
@@ -1,9 +1,69 @@
 import { Injectable } from '@nestjs/common';
+import { Role } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class MembersService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async toggleMembershipStatus() {}
+  async hasAdminRights(userId: string, organisationId: string) {
+    const userIsAdmin = await this.prisma.user.findFirst({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (userIsAdmin?.role === Role.SUPER) return true;
+
+    const membership = await this.prisma.organisationUser.findFirst({
+      where: {
+        userId,
+        organisationId,
+      },
+    });
+
+    return membership?.role == Role.ADMIN;
+  }
+
+  async checkForMember(userId: string, organisationId: string) {
+    const membership = await this.prisma.organisationUser.findFirst({
+      where: {
+        userId,
+        organisationId,
+      },
+    });
+
+    return membership;
+  }
+
+  async addMember(userId: string, organisationId: string) {
+    const memberCheck = await this.checkForMember(userId, organisationId);
+    const rightsCheck = await this.hasAdminRights(userId, organisationId);
+
+    if (check || !rightsCheck) return;
+
+    const membership = await this.prisma.organisationUser.create({
+      data: {
+        userId,
+        organisationId,
+      },
+    });
+
+    return membership;
+  }
+
+  async removeMember(userId: string, organisationId: string) {
+    const rightsCheck = await this.hasAdminRights(userId, organisationId);
+
+    if (!rightsCheck) return;
+
+    const membership = await this.prisma.organisationUser.deleteMany({
+      where: {
+        userId,
+        organisationId,
+      },
+    });
+
+    return membership;
+  }
 }
