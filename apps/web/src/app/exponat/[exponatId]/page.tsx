b2c6d0b494bd6adc5558273f03492e5b2ac7a5a7
drmodun <jan.modun.st@gmail.com>
2024-02-01 23:29:35 +0100

    more style fixes

--- apps/web/src/app/exponat/[exponatId]/page.tsx
+++ apps/web/src/app/exponat/[exponatId]/page.tsx
@@ -2,6 +2,7 @@ import { serverGetExponat } from "@/api/serverExponat";
 import { ExponatExtendedResponse } from "@biosfera/types";
 import classes from "./page.module.scss";
 import ExponatModal from "components/ExponatModal";
+import { PostCard } from "components/PostCard";
 
 const ExponatPage = async ({ params }: { params: any }) => {
   const exponatInfo: ExponatExtendedResponse = await serverGetExponat(
@@ -10,7 +11,17 @@ const ExponatPage = async ({ params }: { params: any }) => {
 
   return (
     <div className={classes.container}>
-      <ExponatModal exponat={exponatInfo} />
+      <div className={classes.modal}>
+        <ExponatModal exponat={exponatInfo} />
+      </div>
+      <div className={classes.posts}>
+        <span className={classes.title}>Objave</span>
+        <div className={classes.postsRow}>
+          {exponatInfo.posts.map((post, index) => (
+            <PostCard post={post} key={index} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
