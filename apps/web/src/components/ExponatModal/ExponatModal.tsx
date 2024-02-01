8f05964a59046305dc2ec16e56397ae360de1296
drmodun <jan.modun.st@gmail.com>
2024-02-01 13:14:48 +0100

    more style fixes

--- apps/web/src/components/ExponatModal/ExponatModal.tsx
+++ apps/web/src/components/ExponatModal/ExponatModal.tsx
@@ -10,68 +10,75 @@ export interface ExponatModalProps {
   exponat: ExponatExtendedResponse;
 }
 
-export const ExponatModal = ({ exponat }: ExponatModalProps) => (
-  <div className={classes.container}>
-    <div className={classes.mainImage}>
-      <Image src={exponat.mainImage} alt={exponat.title} layout="fill"/>
-    </div>
-    <div className={classes.title}>
-      <span className={classes.latinName}>{exponat.alternateName}</span>
-      <span className={classes.name}>{exponat.title}</span>
-    </div>
-    <div className={classes.categorization}></div>
-    <div className={classes.actions}>
-      <div className={classes.button}>
-        <span className={classes.text}>Dodaj u favorite</span>
+export const ExponatModal = ({ exponat }: ExponatModalProps) => {
+  exponat.attributes = JSON.parse(exponat.attributes.toString());
+  return (
+    <div className={classes.container}>
+      <div className={classes.mainImage}>
+        <Image src={exponat.mainImage} alt={exponat.title} layout="fill" />
       </div>
-      <div className={classes.button}>
-        <span className={classes.text}>Generiraj QR kod</span>
-      </div>
-    </div>
-    <div className={classes.section}>
       <div className={classes.title}>
-        <div className={classes.image}>
-          <Image src={desc} alt={exponat.title} layout="fill"/>
-        </div>
-        <span className={classes.titleText}>Kratki opis</span>
+        <span className={classes.latinName}>{exponat.alternateName}</span>
+        <span className={classes.name}>{exponat.title}</span>
       </div>
-      <div className={classes.description}>{exponat.description}</div>
-    </div>
-    <div className={classes.section}>
-      <div className={classes.title}>
-        <div className={classes.image}>
-          <Image src={list} alt={exponat.title} layout="fill"/>
+      <div className={classes.categorization}></div>
+      <div className={classes.actions}>
+        <div className={classes.button}>
+          <span className={classes.text}>Dodaj u favorite</span>
         </div>
-        <span className={classes.titleText}>Fun Facts</span>
+        <div className={classes.button}>
+          <span className={classes.text}>Generiraj QR kod</span>
+        </div>
+      </div>
+      <div className={classes.section}>
+        <div className={classes.title}>
+          <div className={classes.image}>
+            <Image src={desc} alt={exponat.title} layout="fill" />
+          </div>
+          <span className={classes.titleText}>Kratki opis</span>
+        </div>
+        <div className={classes.description}>{exponat.description}</div>
+      </div>
+      <div className={classes.section}>
+        <div className={classes.title}>
+          <div className={classes.image}>
+            <Image src={list} alt={exponat.title} layout="fill" />
+          </div>
+          <span className={classes.titleText}>Fun Facts</span>
+        </div>
         <div className={classes.funFacts}>
           {exponat.funFacts.map((fact, index) => (
             <div className={classes.funFact} key={index}>
               <div className={classes.image}>
-                <Image src={likeLeafGreen} alt={fact} layout="fill"/>
+                <Image src={likeLeafGreen} alt={fact} layout="fill" />
               </div>
               <span className={classes.text}>{fact}</span>
             </div>
           ))}
         </div>
       </div>
-    </div>
-    <div className={classes.section}>
-      <div className={classes.title}>
-        <div className={classes.image}>
-          <Image src={table} alt={exponat.title} />
-        </div>
-        <span className={classes.titleText}>Tablica</span>
-      </div>
-      <div className={classes.table}>
-        {Object.keys(exponat.attributes).map((key, index) => (
-          <div className={classes.tableRow} key={index}>
-            <span className={classes.tableKey}>{stringCapitaliser(key)}</span>
-            <span className={classes.tableValue}>
-              {exponat.attributes[key].toString()}
-            </span>
+      <div className={classes.section}>
+        <div className={classes.title}>
+          <div className={classes.image}>
+            <Image src={table} alt={exponat.title} />
           </div>
-        ))}
+          <span className={classes.titleText}>Tablica</span>
+        </div>
+        <div className={classes.table}>
+          {Object.keys(exponat.attributes).map(
+            (key, index) => (
+              <div className={classes.tableRow} key={index}>
+                <span className={classes.tableKey}>
+                  {stringCapitaliser(key)}
+                </span>
+                <span className={classes.tableValue}>
+                  {exponat.attributes[key].toString()}
+                </span>
+              </div>
+            )
+          )}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
