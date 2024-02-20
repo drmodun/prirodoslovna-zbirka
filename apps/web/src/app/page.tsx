06cf54c00637204793bb254d7fa4d0092f79b334
Lovre Tomić <tomiclovre05@gmail.com>
2024-02-20 18:20:00 +0100

    added second section

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,6 @@
 import c from "./page.module.scss";
 import PlantPicture from "../assets/images/landing-plant.png";
+import ExponatGroupPicture from "../assets/images/exponat-group.png";
 import Image from "next/image";
 import BaseButton from "components/BaseButton";
 import Link from "next/link";
@@ -7,7 +8,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <section className={c.headerHero}>
+      <section className={c.landingSection}>
         <div className={c.content}>
           <h2>Vaša digitalna zbirka.</h2>
           <p>
@@ -19,6 +20,17 @@ export default function Home() {
           </Link>
         </div>
         <Image className={c.plantImage} src={PlantPicture} alt="plant" />
+      </section>
+
+      <section className={c.landingSection}>
+        <div className={c.content}>
+          <h2>Pregledaj eksponate</h2>
+          <p>Pogledaj sadržaj prirodoslovnih zbirki svih organizacija.</p>
+          <Link href="/login">
+            <BaseButton className={c.button} text="Prijavi se" />
+          </Link>
+        </div>
+        <Image className={c.plantImage} src={ExponatGroupPicture} alt="plant" />
       </section>
     </main>
   );
