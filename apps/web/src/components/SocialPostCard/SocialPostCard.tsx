fc36235f6edec3970705c4eb8af5393da6acda32
drmodun <jan.modun.st@gmail.com>
2024-02-22 04:12:49 +0100

    api changes, bugfixes. visual changes and much more

--- apps/web/src/components/SocialPostCard/SocialPostCard.tsx
+++ apps/web/src/components/SocialPostCard/SocialPostCard.tsx
@@ -2,6 +2,7 @@ import { ShortSocialPostResponse } from "@biosfera/types";
 import classes from "./SocialPostCard.module.scss";
 import Image from "next/image";
 import Link from "next/link";
+import ToggelApprovalButton from "components/ToggleApprovalButton";
 
 export interface SocialPostCardProps {
   post: ShortSocialPostResponse;
