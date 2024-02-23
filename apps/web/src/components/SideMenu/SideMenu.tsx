0c948819badc1a7b98dca838fc1578a03cb3fda1
Lovre Tomić <tomiclovre05@gmail.com>
2024-02-23 16:11:16 +0100

    added default data

--- apps/web/src/components/SideMenu/SideMenu.tsx
+++ apps/web/src/components/SideMenu/SideMenu.tsx
@@ -4,33 +4,57 @@ import c from "./SideMenu.module.scss";
 import ProfileImagePlaceholder from "../../assets/images/profile-image-placeholder.png";
 import Image from "next/image";
 import SettingsIcon from "../../assets/icons/settings.svg";
+import UserIcon from "../../assets/icons/user.svg";
+import LogoutIcon from "../../assets/icons/logout.svg";
 import SearchIcon from "../../assets/icons/search.svg";
 import IconButton from "./IconButton";
 
-type SideMenuProps = {
-  isOpen: boolean;
+type UserData = {
+  image: string;
+  profileUrl: string;
+  fullName: string;
+  organisation: string;
+  organisationUrl: string;
 };
 
-const SideMenu = ({ isOpen }: SideMenuProps) => {
+const defaultUser: UserData = {
+  image: "",
+  profileUrl: "",
+  fullName: "Ime Prezime",
+  organisation: "Ime Organizacije",
+  organisationUrl: "",
+};
+
+type SideMenuProps = {
+  isOpen: boolean;
+  user: UserData;
+};
+
+const SideMenu = ({ isOpen, user = defaultUser }: SideMenuProps) => {
   const classes = clsx(c.sideMenu, {
     [c.closed]: !isOpen,
   });
+
   return (
     <div className={classes}>
       <div className={c.profileInfo}>
         <Image
           className={c.image}
-          src={ProfileImagePlaceholder}
+          src={user.image || ProfileImagePlaceholder}
           alt="placeholder"
         />
         <div className={c.userInfoWrapper}>
-          <p className={c.name}>Ivana Ivanović</p>
-          <a className={c.organisation}>III. gimnazija, Split</a>
+          <p className={c.name}>{user.fullName}</p>
+          <a className={c.organisation} href={user.organisationUrl}>
+            {user.organisation}
+          </a>
         </div>
         <div className={c.buttonsWrapper}>
-          <IconButton icon={SettingsIcon} />
-          <IconButton icon={SettingsIcon} />
-          <IconButton icon={SettingsIcon} />
+          <IconButton
+            icon={UserIcon}
+            onClick={() => console.log(user.profileUrl)}
+          />
+          <IconButton icon={LogoutIcon} onClick={() => console.log("logout")} />
         </div>
       </div>
       <div className={c.searchBar}>
