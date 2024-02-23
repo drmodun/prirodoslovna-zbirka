84afb4535f09a712e77062e03f3356e6fb73790b
Lovre Tomić <tomiclovre05@gmail.com>
2024-02-23 15:26:12 +0100

    added side menu content

--- apps/web/src/components/SideMenu/SideMenu.tsx
+++ apps/web/src/components/SideMenu/SideMenu.tsx
@@ -1,6 +1,9 @@
 "use client";
 import clsx from "clsx";
 import c from "./SideMenu.module.scss";
+import ProfileImagePlaceholder from "../../assets/images/profile-image-placeholder.png";
+import Input from "components/Input";
+import Image from "next/image";
 
 type SideMenuProps = {
   isOpen: boolean;
@@ -10,7 +13,27 @@ const SideMenu = ({ isOpen }: SideMenuProps) => {
   const classes = clsx(c.sideMenu, {
     [c.closed]: !isOpen,
   });
-  return <div className={classes}>side menu</div>;
+  return (
+    <div className={classes}>
+      <div className={c.profileInfo}>
+        <Image
+          className={c.image}
+          src={ProfileImagePlaceholder}
+          alt="placeholder"
+        />
+        <div className={c.userInfoWrapper}>
+          <p>Ivana Ivanović</p>
+          <a href="">III. gimnazija, Split</a>
+        </div>
+        <div className={c.buttons}>
+          <button></button>
+          <button></button>
+          <button></button>
+        </div>
+      </div>
+      <input type="text" />
+    </div>
+  );
 };
 
 export default SideMenu;
