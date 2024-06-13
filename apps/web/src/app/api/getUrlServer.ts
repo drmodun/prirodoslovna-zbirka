9a8e507557584b5ff9f33ce32a484d18a104c789
drmodun <jan.modun.st@gmail.com>
2024-06-13 22:25:06 +0200

    fix dumb mistake

--- apps/web/src/app/api/getUrlServer.ts
+++ apps/web/src/app/api/getUrlServer.ts
@@ -1,7 +1,7 @@
 "use server";
 
 export const getBaseUrl = () => {
-  process?.env?.WEB_URL
+  return process?.env?.WEB_URL
     ? process.env.DOCKER
       ? "http://api_container:5500"
       : `${process.env.WEB_URL}/api`
