2f997774596758da3d80a53ac70ec45aa20d3ad7
drmodun <jan.modun.st@gmail.com>
2024-06-13 22:06:33 +0200

    some url fixes

--- apps/web/src/app/api/getUrlServer.ts
+++ apps/web/src/app/api/getUrlServer.ts
@@ -1,6 +1,9 @@
 "use server";
 
-export const getBaseUrl = () =>
-  process?.env?.WEB_URL && process.env.DOCKER
-    ? "http://api_container:5500"
+export const getBaseUrl = () => {
+  process?.env?.WEB_URL
+    ? process.env.DOCKER
+      ? "http://api_container:5500"
+      : `${process.env.WEB_URL}/api`
     : "http://localhost:5500";
+};
