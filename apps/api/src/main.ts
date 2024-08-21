b5db8943f87531e64ad59004384bd00fecb61027
drmodun <jan.modun.st@gmail.com>
2024-08-21 05:02:57 +0200

    More slight fixes

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -25,6 +25,6 @@ async function bootstrap() {
     return int ?? this.toString();
   };
 
-  await app.listen(process.env.API_PORT || 5500);
+  await app.listen(process.env.API_PORT || 3000);
 }
 bootstrap();
